3be4108f5e841f2487a9405b6707d8741dcf933c
johndonga72 <johndonga72@gmail.com>
2026-05-20 22:26:49 +0530

    day 3

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,35 +1,26 @@
 import React from 'react';
-import styles from './page.module.css'
-import Image from 'next/image';
+import styles from "./page.module.css";
 import Button from '../components/button/Button';
-const contact = () => {
+import Image from 'next/image';
+
+const Contact = () => {
     return (
         <div className={styles.container}>
-            <div className={styles.imgContainer}>
-                <Image src="/contact.png" alt="" fill={true} className={styles.image} />
-                <div className={styles.img}>
-                    <h1 className={styles.imgTitle}>Digital Storytellers</h1>
-                    <h2 className={styles.imgDesc}>Handcrafting award winning digital experiences</h2>
+            <h1 className={styles.title}>Let's keep in Touch</h1>
+            <div className={styles.content}>
+                <div className={styles.imgContainer}>
+                    <Image src='/contact.png' alt='' fill={true} className={styles.image} />
                 </div>
-            </div>
-            <div className={styles.textContainer}>
-                <div className={styles.item}>
-                    <h1>Who Are you ?</h1>
-                    <p>
-                        We are a team of passionate professionals dedicated to creating exceptional digital experiences.
-                        With a blend of creativity, technical expertise, and a commitment to excellence, we strive to bring your vision to life in the digital realm.
+                <form className={styles.form}>
+                    <input type='text' placeholder='Name' className={styles.input} />
+                    <input type='text' placeholder='Email' className={styles.input} />
+                    <textarea placeholder='Message' className={styles.textArea} cols="30" rows="10" />
+                    <Button url="#" text="send" />
+                </form>
 
-                    </p>
-                </div>
-                <div className={styles.item}>
-                    <h1 className={styles.title}>What Do We Do ?</h1>
-                    <p>
-                        We specialize in crafting award-winning digital experiences that captivate audiences and drive results. From innovative web design and development to immersive multimedia solutions, we leverage cutting-edge technologies and creative storytelling to deliver impactful digital solutions that exceed expectations.
-                    </p>
-                </div>
-                <Button url="contact" text="Contact Us" />
             </div>
+
         </div>
-    );
-};
-export default contact
+    )
+}
+export default Contact;
--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import styles from './page.module.css';
+import Link from "next/link";
 const portfolio = () => {
     return (
-        <div>portfolio</div>
+        <div className={styles.container}>
+            <h1 className={styles.selectTitle}>My Portfolio</h1>
+            <div className={styles.items}>
+                <Link href="/portfolio/illustrations" className={styles.projectLink}>
+                    <span className={styles.title}>Illustrations</span>
+                </Link>
+                <Link href="/portfolio/websites" className={styles.projectLink}>
+                    <span className={styles.title}>Websites</span>
+                </Link>
+                <Link href="/portfolio/applications" className={styles.projectLink}>
+                    <span className={styles.title}>Applications</span>
+                </Link>
+            </div>
+        </div>
     )
 }
 export default portfolio
